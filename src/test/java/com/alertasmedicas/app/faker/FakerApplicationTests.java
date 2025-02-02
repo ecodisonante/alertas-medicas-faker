1effd4a2804ecb3a34464491c17a87de7e68ef97
Francisco Valdes <ecodisonante@gmail.com>
2025-02-02 00:25:43 -0300

    initial commit

---
+++ src/test/java/com/alertasmedicas/app/faker/FakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertasmedicas.app.faker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
