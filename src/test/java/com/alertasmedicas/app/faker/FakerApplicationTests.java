26e36a88fb3fc0ba0cf4066be41b65d87c79a87d
Francisco Valdes <ecodisonante@gmail.com>
2025-02-03 12:56:35 -0300

    deshabilita test para no gatillar el init de FakerService

--- src/test/java/com/alertasmedicas/app/faker/FakerApplicationTests.java
+++ src/test/java/com/alertasmedicas/app/faker/FakerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FakerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
